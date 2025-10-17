2703420789488e2944c6a929255579db6a6db338
Shivani Patel <shivani@mac.dhcp.asu.edu>
2025-10-17 16:44:02 -0700

    add user input switch logic

--- Project.java
+++ Project.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Project {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+        int userChoice = -1;
+        int userInput1;
+        int sum = -1, fac = -1, leftmost = -1;
+        int length;
+
+        do {
+            userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+            case 1:
+                System.out.println("Enter a number:");
+                userInput1 = scanner.nextInt();
+                break;
+            case 2:
+                System.out.println("Enter a number:");
+                userInput1 = scanner.nextInt();
+                break;
+            case 3:
+                System.out.println("Enter the length of sequence");
+                length = scanner.nextInt();
+                System.out.println("Enter the sequence");
+                int oddCount = 0;
+                break;
+            case 4:
+                System.out.println("Enter a number:");
+                userInput1 = scanner.nextInt();
+                break;
+            case 5:
+                System.out.println("Enter the first integer: ");
+                int x = scanner.nextInt();
+                System.out.println("Enter the second integer: ");
+                int y = scanner.nextInt();
+                break;
+            case 6:
+                System.out.println("Bye");
+                break;
+            }
+        } while (userChoice != 6);
     }
 }
